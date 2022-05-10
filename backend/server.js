f386012567e77e26cfe6bff5e93eae4add6c1050
NitishMatiyara <matiyaranitish@gmail.com>
2022-05-10 19:05:02 +0530

    make it better

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ app.use("/api/message", messageRoutes);
 const __dirname1 = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "frontend/build")));
+  app.use(express.static(path.join(__dirname1, "/app/frontend/build")));
 
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname1, "app", "frontend", "build", "index.html"))
