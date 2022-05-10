e94c600f5de98cdef1d03ebe181af5dd64222f13
NitishMatiyara <matiyaranitish@gmail.com>
2022-05-10 23:47:11 +0530

    make it better

--- backend/server.js
+++ backend/server.js
@@ -33,7 +33,13 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname1, "/frontend/build")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html", function (err) {
+     if(err){
+       next(err)
+     } else {
+       console.log("sent:", "index.html")
+     }
+    }))
   );
 } else {
   app.get("/", (req, res) => {
