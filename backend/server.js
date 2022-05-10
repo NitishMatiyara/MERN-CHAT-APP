d7423bef31cea88c68e96cc923a2582cde8eb44a
NitishMatiyara <matiyaranitish@gmail.com>
2022-05-10 19:14:23 +0530

    save

--- backend/server.js
+++ backend/server.js
@@ -29,10 +29,10 @@ app.use("/api/message", messageRoutes);
 const __dirname1 = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "/app/frontend/build")));
+  app.use(express.static(path.join(__dirname1, "/frontend/public")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "app", "frontend", "build", "index.html"))
+    res.sendFile(path.resolve(__dirname1, "frontend", "public", "index.html"))
   );
 } else {
   app.get("/", (req, res) => {
