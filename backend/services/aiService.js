d8c6b94eaa087b1fcdf3014da32fe8a15b26d499
Nitish Matiyara <91205231+NitishMatiyara@users.noreply.github.com>
2026-03-13 20:07:11 +0530

    Update aiService.js

--- backend/services/aiService.js
+++ backend/services/aiService.js
@@ -2,6 +2,9 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
+const models = await genAI.listModels();
+console.log(models);
+
 const model = genAI.getGenerativeModel({
   model: "gemini-2.0-flash",
 });
